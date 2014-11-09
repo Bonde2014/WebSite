80603c1593441911923f8193daed779441530913
wjf1987 <magister.feng@gmail.com>
2014-11-09 15:45:16 +0800

    商品分级报价服务

---
+++ src/main/java/cn/com/bonde/b2b/website/service/IXsFjbjService.java
@@ -0,0 +1,7 @@
+package cn.com.bonde.b2b.website.service;
+
+public interface IXsFjbjService {
+    
+    public List
+
+}
---
+++ src/main/java/cn/com/bonde/b2b/website/service/impl/XsFjbjServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.com.bonde.b2b.website.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import cn.com.bonde.b2b.website.service.IXsFjbjService;
+
+@Service(value="xsFjbjService")
+public class XsFjbjServiceImpl implements IXsFjbjService {
+
+}
